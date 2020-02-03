85dac30a37952644c677a0c30c16aa995ad50e7d
Hammertonmutuku <lthamma3@gmail.com>
2020-02-03 05:27:51 +0300

    added the on click listeners and intents

--- app/src/main/java/com/projects/muza/MainActivity.java
+++ app/src/main/java/com/projects/muza/MainActivity.java
@@ -2,13 +2,28 @@ package com.projects.muza;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mFindMusicButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFindMusicButton = (Button)findViewById(R.id.findMusicButton);
+        mFindMusicButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+                public void onClick(View v) {
+            Intent intent = new Intent(MainActivity.this, genresActivity.class);
+            startActivity(intent);
+        }
+
+        }
     }
-}
+
