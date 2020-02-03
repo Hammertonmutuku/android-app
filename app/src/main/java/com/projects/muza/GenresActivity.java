79afbc4c4995e031e6268392e6f924f8192347aa
Hammertonmutuku <lthamma3@gmail.com>
2020-02-03 10:12:45 +0300

    implimented list

--- app/src/main/java/com/projects/muza/GenresActivity.java
+++ app/src/main/java/com/projects/muza/GenresActivity.java
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class GenresActivity extends AppCompatActivity {
     public class RestaurantsActivity extends AppCompatActivity {
@@ -28,8 +31,18 @@ public class GenresActivity extends AppCompatActivity {
             ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, muzas);
             mListView.setAdapter(adapter);
 
+            mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int i, long 1) {
+                    String muza = ((TextView) view).getText().toString();
+                    Toast.makeText(GenresActivity.this, muza, Toast.LENGTH_LONG).show();
+                }
+            });
+
             Intent intent = getIntent();
             String muza = intent.getStringExtra("muza");
             mLocationTextView.setText("Here are all the restaurants near: " + muza);
         }
     }
+
+}
