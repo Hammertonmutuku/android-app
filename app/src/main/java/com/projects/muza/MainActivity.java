3d3f257371b0e693f9e50311dd6d6c63af7d1fb1
Hammertonmutuku <lthamma3@gmail.com>
2020-02-03 05:28:35 +0300

    made changes on the intent

--- app/src/main/java/com/projects/muza/MainActivity.java
+++ app/src/main/java/com/projects/muza/MainActivity.java
@@ -6,16 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     private Button mFindMusicButton;
-
+    private EditText mLocationEditView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        mLocationEditView = (EditText) findViewById(R.id.LocationEditView);
         mFindMusicButton = (Button)findViewById(R.id.findMusicButton);
         mFindMusicButton.setOnClickListener(new View.OnClickListener() {
         @Override
