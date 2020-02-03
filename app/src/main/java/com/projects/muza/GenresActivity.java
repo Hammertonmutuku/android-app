a580ce36721d3d329fd26e49c67ef8c07c8b3c20
Hammertonmutuku <lthamma3@gmail.com>
2020-02-03 06:08:20 +0300

    added list view

--- app/src/main/java/com/projects/muza/GenresActivity.java
+++ app/src/main/java/com/projects/muza/GenresActivity.java
@@ -4,8 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class GenresActivity extends AppCompatActivity {
+    private ListView mListView;
+    private TextView mLocationTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +18,6 @@ public class GenresActivity extends AppCompatActivity {
         setContentView(R.layout.activity_genres);
         Intent intent = getIntent();
         String muza = intent.getStringExtra("muza");
+        mLocationTextView.setText("Here are all the restaurants near: " + muza);
     }
 }
