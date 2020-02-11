c36031ad05bc0c3651ef81eeed9adb247dd0aa12
Hammertonmutuku <lthamma3@gmail.com>
2020-02-11 21:52:53 +0300

    initialized sting constants

--- app/src/main/java/com/projects/muza/Constants.java
+++ app/src/main/java/com/projects/muza/Constants.java
@@ -1,4 +1,6 @@
 package com.projects.muza;
 
 public class Constants {
+    public static final String last_API_KEY_URL = "https://www.last.fm/api/";
+    public static final String lastfm_API_KEY = BuildConfig.last_API_KEY;
 }
