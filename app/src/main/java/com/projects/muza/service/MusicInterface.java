a37bf5736ec80c641d194393cb4375ac10edbaa3
Hammertonmutuku <lthamma3@gmail.com>
2020-02-12 11:07:22 +0300

    deleted the previous interface

--- app/src/main/java/com/projects/muza/service/MusicInterface.java
+++ app/src/main/java/com/projects/muza/service/MusicInterface.java
@@ -1,4 +1,15 @@
 package com.projects.muza.service;
 
+import com.projects.muza.models.Album;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface MusicInterface {
+
+    @GET("/Album")
+    Call<List<Album>> getAlbumsTitle();
+
 }
