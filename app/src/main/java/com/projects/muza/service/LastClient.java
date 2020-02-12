66b8fa2b518b5f601d8ded4d69ae67ce59813ce7
Hammertonmutuku <lthamma3@gmail.com>
2020-02-12 09:14:11 +0300

    added a new list activity

--- app/src/main/java/com/projects/muza/service/LastClient.java
+++ app/src/main/java/com/projects/muza/service/LastClient.java
@@ -1,4 +1,44 @@
 package com.projects.muza.service;
 
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import static com.projects.muza.BuildConfig.last_API_KEY;
+import static com.projects.muza.Constants.last_API_KEY_URL;
+
 public class LastClient {
+    private static Retrofit retrofit = null;
+
+    public static lastClient getTrackInfo() {
+        if (retrofit == null) {
+            OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                    .addInterceptor(new Interceptor() {
+                        @Override
+                        public Response intercept(Chain chain) throws IOException {
+                            Request newRequest  = chain.request().newBuilder()
+                                    .addHeader("Authorization", last_API_KEY)
+                                    .build();
+                            return chain.proceed(newRequest);
+                        }
+                    })
+                    .build();
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(last_API_KEY_URL)
+                    .client(okHttpClient)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+
+        return retrofit.create(lastfm.class);
+    }
+}
+        }
+    }
+
 }
